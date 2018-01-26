e06ebe8dfa330ae4f8b15841c63410bdafe654fa
iXenome <secretomega01@gmail.com>
2018-01-26 13:28:53 +0800

    Initial Commit

---
+++ src/pages/paypal/paypal.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-paypal',
+  templateUrl: 'paypal.html'
+})
+export class PayPalPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
